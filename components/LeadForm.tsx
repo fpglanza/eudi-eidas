a8e0d53e9d1b4f1cc320508cc5f24e14d1712909
Filo Lanza <fpg.lanza@gmail.com>
2025-11-06 15:43:06 +0100

    update: vercel preview fix

--- components/LeadForm.tsx
+++ components/LeadForm.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 export default function LeadForm() {
-  const src = process.env.NEXT_PUBLIC_LEAD_IFRAME_SRC;
+  const src = process.env.NEXT_PUBLIC_LEAD_IFRAME_SRC ??
+  "https://embeds.beehiiv.com/10f8b9c5-ed24-4fab-8a06-d0c8fc51012f?slim=true";
+
 
   if (!src) return null;
 
